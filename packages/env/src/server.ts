94fd5c4d961c57feed4765b2f293a8532217afb7
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2026-09-16 07:52:33 +0200

    feat(env): remove database url variable from global variables

--- packages/env/src/server.ts
+++ packages/env/src/server.ts
@@ -4,11 +4,9 @@ import * as z from "zod";
 export const globalEnv = createEnv({
 	server: {
 		PORT: z.string(),
-		DATABASE_URL: z.url(),
 	},
 
 	runtimeEnvStrict: {
-		DATABASE_URL: process.env.DATABASE_URL,
 		PORT: process.env.PORT,
 	},
 	emptyStringAsUndefined: true,
