a549e8af249df0a4d3babd81a2d53b87d8b645b7
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2026-09-18 06:50:25 +0200

    feat(auth): add env validation

---
+++ apps/auth/env.server.ts
@@ -0,0 +1,14 @@
+import { createNextjsEnv } from "@workspace/env";
+import { globalEnv } from "@workspace/env/server";
+import * as z from "zod/v3";
+
+export const env = createNextjsEnv({
+	extends: [globalEnv],
+	server: {
+		BETTER_AUTH_URL: z.string().url(),
+		BETTER_AUTH_SECRET: z
+			.string()
+			.min(32, "Must be at least 32 characters long"),
+	},
+	experimental__runtimeEnv: process.env,
+});
