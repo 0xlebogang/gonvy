4274139ddf3c96aaa0c4cbe20ed11c881e6916e5
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2026-09-18 18:14:23 +0200

    feat(platform): add dedicated ui shell component

--- apps/platform/app/providers.tsx
+++ apps/platform/app/providers.tsx
@@ -1,6 +1,11 @@
+import { SidebarProvider } from "@workspace/ui/components/sidebar";
 import type React from "react";
 import { ThemeProvider } from "@/components/theme-provider";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-	return <ThemeProvider>{children}</ThemeProvider>;
+	return (
+		<ThemeProvider>
+			<SidebarProvider>{children}</SidebarProvider>
+		</ThemeProvider>
+	);
 }
---
+++ apps/platform/components/ui-shell.tsx
@@ -0,0 +1,39 @@
+import {
+	Breadcrumb,
+	BreadcrumbItem,
+	BreadcrumbLink,
+	BreadcrumbList,
+	BreadcrumbPage,
+	BreadcrumbSeparator,
+} from "@workspace/ui/components/breadcrumb";
+import { Separator } from "@workspace/ui/components/separator";
+import { SidebarTrigger } from "@workspace/ui/components/sidebar";
+import type * as React from "react";
+
+export default function UIShell({ children }: { children: React.ReactNode }) {
+	return (
+		<>
+			<header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear">
+				<div className="flex items-center gap-2 px-4">
+					<SidebarTrigger className="-ml-1" />
+					<Separator
+						orientation="vertical"
+						className="mr-2 data-vertical:h-4 data-vertical:self-auto"
+					/>
+					<Breadcrumb>
+						<BreadcrumbList>
+							<BreadcrumbItem className="hidden md:block">
+								<BreadcrumbLink href="#">Build Your Application</BreadcrumbLink>
+							</BreadcrumbItem>
+							<BreadcrumbSeparator className="hidden md:block" />
+							<BreadcrumbItem>
+								<BreadcrumbPage>Data Fetching</BreadcrumbPage>
+							</BreadcrumbItem>
+						</BreadcrumbList>
+					</Breadcrumb>
+				</div>
+			</header>
+			<div className="flex flex-1 flex-col gap-4 p-4 pt-0">{children}</div>
+		</>
+	);
+}
