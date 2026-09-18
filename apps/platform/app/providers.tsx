6803def301bf9787d9106acc42a4dfe6ffa0aef1
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2026-09-18 14:43:09 +0200

    feat(platform): add boilerplate sidebar code

---
+++ apps/platform/app/providers.tsx
@@ -0,0 +1,6 @@
+import type React from "react";
+import { ThemeProvider } from "@/components/theme-provider";
+
+export default function Providers({ children }: { children: React.ReactNode }) {
+	return <ThemeProvider>{children}</ThemeProvider>;
+}
